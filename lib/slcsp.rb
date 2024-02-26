6194737f6b3135cfecfca61bf92ad498004f2cb0
Spencer Lucas <jspencerlucas@gmail.com>
2024-02-26 16:36:46 -0500

    Add SLCSP module

--- lib/slcsp.rb
+++ lib/slcsp.rb
@@ -1,11 +1,13 @@
 module SLCSP
-  def self.call
-    true
+  def self.call(rates)
+    rates.length > 1 ? rates.sort[1] : nil
   end
 
-  private
-
-  def seed
-    
+  def self.format(maybe_rate)
+    if maybe_rate
+      sprintf("%.2f", maybe_rate.round(2))
+    else
+      nil
+    end
   end
 end
--- spec/slcsp_spec.rb
+++ spec/slcsp_spec.rb
@@ -1,7 +1,38 @@
 require 'slcsp'
 
 describe "SLCSP" do
-  subject { SLCSP.call() }
+  describe ".call" do
+    subject { SLCSP.call(rates) }
 
-  it { is_expected.to eq(true) }
+    context "with no rates" do
+      let(:rates) { [] }
+      it { is_expected.to eq(nil) }
+    end
+
+    context "with one rate" do
+      let(:rates) { [123.32] }
+      it { is_expected.to eq(nil) }
+    end
+
+    context "with two rates" do
+      let(:rates) { [60.23, 90.12] }
+      it { is_expected.to eq(90.12) }
+    end
+
+    context "with many rates" do
+      let(:rates) { [60.23, 123.43, 90.12, 123.32] }
+      it { is_expected.to eq(90.12) }
+    end
+  end
+
+  describe ".format" do
+    it "returns float rounded 2 decimal places as string" do
+      expect(SLCSP.format(123.332)).to eq("123.33")
+      expect(SLCSP.format(123.339)).to eq("123.34")
+    end
+
+    it "returns nil if given nil" do
+      expect(SLCSP.format(nil)).to eq(nil)
+    end
+  end
 end
